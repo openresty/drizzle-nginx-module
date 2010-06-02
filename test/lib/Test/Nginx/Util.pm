b634cf75511cc26907ca88fa74f555cbc22aa192
agentzh (章亦春) <agentzh@gmail.com>
2010-06-02 18:42:45 +0800

    updated Test::Nginx to enable valgrind support.

--- test/lib/Test/Nginx/Util.pm
+++ test/lib/Test/Nginx/Util.pm
@@ -13,6 +13,7 @@ use HTTP::Response;
 use Module::Install::Can;
 use Cwd qw( cwd );
 use List::Util qw( shuffle );
+use Time::HiRes qw( sleep );
 
 our $NoNginxManager = 0;
 our $Profiling = 0;
@@ -20,9 +21,17 @@ our $Profiling = 0;
 our $RepeatEach = 1;
 our $MAX_PROCESSES = 10;
 
+our $NoShuffle = 0;
+
+our $UseValgrind = $ENV{TEST_NGINX_USE_VALGRIND};
+
+sub no_shuffle () {
+    $NoShuffle = 1;
+}
+
 our $ForkManager;
 
-if ($Profiling) {
+if ($Profiling || $UseValgrind) {
     eval "use Parallel::ForkManager";
     if ($@) {
         die "Failed to load Parallel::ForkManager: $@\n";
@@ -36,7 +45,8 @@ our $LogLevel               = 'debug';
 our $MasterProcessEnabled   = 'off';
 our $DaemonEnabled          = 'on';
 our $ServerPort             = 1984;
-our $ServerPortForClient    = 1984;
+our $ServerPortForClient    = $ENV{TEST_NGINX_CLIENT_PORT} || 1984;
+our $NoRootLocation = 0;
 #our $ServerPortForClient    = 1984;
 
 
@@ -54,6 +64,10 @@ sub worker_connections (@) {
     } else {
         return $WorkerConnections;
     }
+}
+
+sub no_root_location () {
+    $NoRootLocation = 1;
 }
 
 sub workers (@) {
@@ -110,10 +124,12 @@ our @EXPORT_OK = qw(
     repeat_each
     master_process_enabled
     log_level
+    no_shuffle
+    no_root_location
 );
 
 
-if ($Profiling) {
+if ($Profiling || $UseValgrind) {
     $DaemonEnabled          = 'off';
     $MasterProcessEnabled   = 'off';
 }
@@ -148,13 +164,13 @@ sub run_tests () {
         #warn "[INFO] Using nginx version $NginxVersion ($NginxRawVersion)\n";
     }
 
-    for my $block (shuffle Test::Base::blocks()) {
+    for my $block ($NoShuffle ? Test::Base::blocks() : shuffle Test::Base::blocks()) {
         #for (1..3) {
             run_test($block);
         #}
     }
 
-    if ($Profiling) {
+    if ($Profiling || $UseValgrind) {
         $ForkManager->wait_all_children;
     }
 }
@@ -213,11 +229,11 @@ http {
     default_type text/plain;
     keepalive_timeout  68;
 
-    $http_config
+$http_config
 
     server {
         listen          $ServerPort;
-        server_name     localhost;
+        server_name     'localhost';
 
         client_max_body_size 30M;
         #client_body_buffer_size 4k;
@@ -230,10 +246,18 @@ $ConfigPreamble
 $config
         # End test case config.
 
+_EOC_
+
+    if (! $NoRootLocation) {
+        print $out <<_EOC_;
         location / {
             root $HtmlDir;
             index index.html index.htm;
         }
+_EOC_
+    }
+
+    print $out <<_EOC_;
     }
 }
 
@@ -420,16 +444,38 @@ start_nginx:
                 $cmd = "nginx -c $ConfFile > /dev/null";
             }
 
-            if ($Profiling) {
+            if ($UseValgrind) {
+                if (-f 'valgrind.suppress') {
+                    $cmd = "valgrind -q --leak-check=full --gen-suppressions=all --suppressions=valgrind.suppress $cmd";
+                } else {
+                    $cmd = "valgrind -q --leak-check=full --gen-suppressions=all $cmd";
+                }
+
+                warn "$name\n";
+                #warn "$cmd\n";
+            }
+
+            if ($Profiling || $UseValgrind) {
                 my $pid = $ForkManager->start;
                 if (!$pid) {
                     # child process
+                    exec $cmd;
+
+=begin cmt
+
                     if (system($cmd) != 0) {
                         Test::More::BAIL_OUT("$name - Cannot start nginx using command \"$cmd\".");
                     }
 
                     $ForkManager->finish; # terminate the child process
+
+=end cmt
+
+=cut
+
                 }
+                #warn "sleeping";
+                sleep 1;
             } else {
                 if (system($cmd) != 0) {
                     Test::More::BAIL_OUT("$name - Cannot start nginx using command \"$cmd\".");
@@ -437,6 +483,13 @@ start_nginx:
             }
 
             sleep 0.1;
+        }
+    }
+
+    if ($block->init) {
+        eval $block->init;
+        if ($@) {
+            Test::More::BAIL_OUT("$name - init failed: $@");
         }
     }
 
@@ -459,12 +512,39 @@ start_nginx:
         }
     }
 
-    if (defined $block->quit && $Profiling) {
-        warn "Found quit...";
+    if ($Profiling || $UseValgrind) {
+        #warn "Found quit...";
         if (-f $PidFile) {
+            #warn "found pid file...";
             my $pid = get_pid_from_pidfile($name);
             if (system("ps $pid > /dev/null") == 0) {
                 write_config_file($config, $block->http_config);
+                if (kill(SIGQUIT, $pid) == 0) { # send quit signal
+                    warn("$name - Failed to send quit signal to the nginx process with PID $pid");
+                }
+                sleep 0.1;
+                if (-f $PidFile) {
+                    #warn "killing with force (valgrind or profile)...\n";
+                    kill(SIGKILL, $pid);
+                    sleep 0.02;
+                } else {
+                    #warn "nginx killed";
+                }
+            } else {
+                unlink $PidFile or
+                    die "Failed to remove pid file $PidFile\n";
+            }
+        } else {
+            #warn "pid file not found";
+        }
+    }
+}
+
+END {
+    if ($UseValgrind) {
+        if (-f $PidFile) {
+            my $pid = get_pid_from_pidfile('');
+            if (system("ps $pid > /dev/null") == 0) {
                 if (kill(SIGQUIT, $pid) == 0) { # send quit signal
                     #warn("$name - Failed to send quit signal to the nginx process with PID $pid");
                 }
@@ -475,8 +555,7 @@ start_nginx:
                     sleep 0.02;
                 }
             } else {
-                unlink $PidFile or
-                    die "Failed to remove pid file $PidFile\n";
+                unlink $PidFile;
             }
         }
     }
