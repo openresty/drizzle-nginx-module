82d819054f618afff5b8a778486b03687d159adc
Piotr Sikora <piotr.sikora@frickle.com>
2010-06-13 13:59:50 +0000

    Restrict "drizzle_pass" to "location" and "if" context, otherwise it would segfault.

--- src/ngx_http_drizzle_module.c
+++ src/ngx_http_drizzle_module.c
@@ -56,8 +56,7 @@ static ngx_command_t ngx_http_drizzle_cmds[] = {
         NULL },
     {
         ngx_string("drizzle_pass"),
-        NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF
-            |NGX_HTTP_LIF_CONF|NGX_CONF_TAKE1,
+        NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF|NGX_CONF_TAKE1,
         ngx_http_drizzle_pass,
         NGX_HTTP_LOC_CONF_OFFSET,
         0,
